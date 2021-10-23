95e7d9722b017c572cf0b3333863140dd264101b
Luís <fnsc.luis@gmail.com>
2021-10-23 15:41:23 -0300

    definido método para tocar música

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   // code here
+  const song = document.getElementsByClassName('song')[0];
+
+  const icon = document.getElementsByClassName('content-container__play-button')[0];
+  const pause_image_file = 'assets/images/pause-min.png';
+  const play_image_file = 'assets/images/play-min.png';
+
+  let isPlay;
+
+  icon.addEventListener('click', () => {
+    isPlay = song.paused;
+
+    if (isPlay) {
+      song.play();
+      icon.src = pause_image_file;
+    } else {
+      song.pause();
+      icon.src = play_image_file;
+    }
+  });
 });
